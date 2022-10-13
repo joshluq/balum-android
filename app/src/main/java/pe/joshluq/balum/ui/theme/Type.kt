18a4634e824995607fcbbb90304f0fae44329561
Josh Luque <josh.luque@scotiabank.com.pe>
2022-10-13 10:12:21 -0500

    feat : add fonts

--- app/src/main/java/pe/joshluq/balum/ui/theme/Type.kt
+++ app/src/main/java/pe/joshluq/balum/ui/theme/Type.kt
@@ -1,18 +1,13 @@
 package pe.joshluq.balum.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import pe.joshluq.balum.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
+    defaultFontFamily = FontFamily(Font(R.font.titillium_web_regular)),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
